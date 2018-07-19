603e05acca68726589b97f10691e391ef46f6e61
Chloe De Guzman <chloedeguzman@minint-j3blm94.corporate.thoughtworks.com>
2018-07-19 15:07:24 -0500

    Added library test cases and library

--- src/com/twu/biblioteca/Library.java
+++ src/com/twu/biblioteca/Library.java
@@ -1,4 +1,9 @@
 package com.twu.biblioteca;
 
+import java.util.ArrayList;
+
 public class Library {
+    ArrayList bookList = new ArrayList();
+
+    //public ArrayList listBooks();
 }
--- test/com/twu/biblioteca/LibraryTest.java
+++ test/com/twu/biblioteca/LibraryTest.java
@@ -1,4 +1,23 @@
 package com.twu.biblioteca;
 
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+
 public class LibraryTest {
+    private Library testLibrary = new Library();
+
+    @Test
+    public void shouldCreateEmptyBookList() {
+        assertTrue(testLibrary.bookList.isEmpty());
+    }
+
+    @Test
+    @Ignore
+    public void shouldShowBookDetails() {
+        // implement me
+    }
+
 }
