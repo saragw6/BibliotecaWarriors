bd88972176f32faff2920d694d31ada50a943427
kaiholnes <kaihol42@gmail.com>
2018-07-20 14:04:18 -0500

    Oops, backward testings

--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -1,14 +1,35 @@
 package com.twu.biblioteca;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.io.PrintStream;
+import java.util.Scanner;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class BibliotecaAppTest {
 
+    private PrintStream printStream;
+    private Scanner scanner;
+    private BibliotecaApp app;
 
+    @Before
+    public void setUp(){
+        app = new BibliotecaApp();
+        printStream = mock(PrintStream.class);
+        scanner = mock(Scanner.class);
+    }
     @Test
-    public void mainMenuTest(){
+    public void listBooksTest(){
+        app.main(null);
+        when(scanner.nextLine()).thenReturn("list books");
+        verify(printStream).print("");
+
 
     }
 
